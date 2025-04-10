3e043095327a588e29f337b12f75f3e04cf83d63
ShreyaRai20 <shreya200199@gmail.com>
2025-04-10 15:22:16 -0500

    added router folder with auth.py and init

--- fastapi/api/deps.py
+++ fastapi/api/deps.py
@@ -32,10 +32,10 @@ async def get_current_user(token: oauth2_bearer_dependancy):
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
         if username is None or user_id is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Coulf not validate user')
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
         return {'username': username, 'id': user_id}
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Coulf not validate user')
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
     
 
 user_dependency =  Annotated[dict, Depends(get_current_user)]
